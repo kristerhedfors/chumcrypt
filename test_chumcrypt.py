89263a74dbbfc2fd2cdf136e4d46eccb32487d53
Krister <user@zeroknight.hacka.re>
2017-08-20 22:16:34 +0200

    test updates

--- test_chumcrypt.py
+++ test_chumcrypt.py
@@ -84,6 +84,7 @@ class Test_SecretBox2(unittest.TestCase):
     def _recursive_boxes(self, keygen, n):
         boxes = []
         keys = [keygen for _ in xrange(n)]
+        print keys
         packages = []
         for i in xrange(n):
             box = SecretBox(keys[i])
