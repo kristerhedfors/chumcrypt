38f9eda6eb5df6bc811ccf12d217446b91fd2438
Krister <user@zeroknight.hacka.re>
2017-08-20 23:05:27 +0200

    major test updates

--- test_chumcrypt.py
+++ test_chumcrypt.py
@@ -9,9 +9,11 @@ import logging
 # import hashlib
 # import hmac
 # import struct
-import StringIO
 from itertools import imap
+from itertools import izip
 from hashlib import sha256
+import random
+import array
 
 from chumcrypt import ChumCipher
 from chumcrypt import SecretBox
@@ -35,22 +37,14 @@ def warn(*args, **kw):
 
 class Test_ChumCipher(unittest.TestCase):
 
-    def test_basics(self):
-        f = StringIO.StringIO('perkele')
-        key = utils.gen_key()
-        assert len(key) == 32
-        nonce = utils.random(16)
-        cc = ChumCipher(key, nonce, f)
-        # print(repr(cc.read(29)))
-
     def test_longer_irregular_read_lengths(self):
         key = 'a' * 32
         nonce = 'n' * 16
         cc = ChumCipher(key, nonce)
         n = 0
-        while n < 2000:
+        while n < 2**12:
             assert len(cc._read_chum(n)) == n
-            n += 11
+            n += 31337
 
 
 class Test_SecretBox(unittest.TestCase):
@@ -78,11 +72,44 @@ class Test_SecretBox(unittest.TestCase):
 
 class Test_SecretBox2(unittest.TestCase):
 
-    def _crypt_decrypt(self, key=None, nonce=None):
-        key = key or utils.random(32)  # faster
-        nonce = key or utils.random(16)  # faster
+    def _inc_one_random_byte(self, s):
+        a = array.array('B', s)
+        i = random.randint(0, len(a) - 1)
+        a[i] = (a[i] + 1) % 0xff
+        return a.tostring()
+
+    def _get_multilayer_cipher(self, keys, msg):
+        '''
+        Chain len(keys) ChumCiphers on top of each other.
+        Return the outmost cipher object.
+        '''
+        n = len(keys)
+        nonsense = [sha256(key).digest() for key in keys]
+        ciphers = []
+        i = 0
+        for (key, nonce) in izip(keys, nonsense):
+            if i > 0:
+                msg = ciphers[i - 1]
+            ciphers += [ChumCipher(key, nonce, msg)]
+        return ciphers[-1]
+
+    def test_multilayer_cipher(self):
+        '''
+        Create a list of keys and two equal multi-layered ciphers from these
+        keys.
+        Send one message through both chains and finally validate integrity.
+        Perform XOR-schism.
+        '''
+        msg = 'Welcome! how did you get here?'
+        keys = [utils.random(32) for _ in xrange(100)]
+        m1 = self._get_multilayer_cipher(keys, msg)
+        ciphertext = m1.read(len(msg))
+        m2 = self._get_multilayer_cipher(keys, ciphertext)
+        assert m2.read(len(msg)) == msg
+        print(msg)
 
     def _recursive_boxes(self, keygen, n):
+        orig_msg = 'Hello there, you again?!'
         boxes = []
         keys = [k for k in keygen]
         packages = []
@@ -94,7 +121,7 @@ class Test_SecretBox2(unittest.TestCase):
         for i in xrange(n):
             box = SecretBox(keys[i])
             if i == 0:
-                msg = 'Welcome! how did you get here?'
+                msg = orig_msg
             else:
                 msg = packages[i-1]
             p = box.encrypt(msg)
@@ -109,21 +136,27 @@ class Test_SecretBox2(unittest.TestCase):
         for i in xrange(len(packages)):
             p = packages[i]
             for key in keys[i:]:
+                #
+                # For each package, assert minor change causes HMAC validation
+                # failure and thus an AssertionException.
+                #
+                box = SecretBox(key)
+                try:
+                    q = self._inc_one_random_byte(p)
+                    box.decrypt(q)
+                except AssertionError:
+                    pass
+                else:
+                    raise Exception('Seal broken! more vet bills..')
                 p = SecretBox(key).decrypt(p)
-            assert p == 'Welcome! how did you get here?'
-        assert p == 'Welcome! how did you get here?'
+            assert p == orig_msg
+        assert p == orig_msg
         print(p)
 
     def test_recursive_boxes(self):
         n = 20
         keygen = imap(lambda i: sha256(str(i)).digest(), xrange(n))
         self._recursive_boxes(keygen, n)
-
-
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
